81277b0bd713638092ad3bd647832908a7f66b5a
Aleksandr-Kondratskii <aleksandr_kondratskii@epam.com>
2019-03-28 16:14:38 +0400

    end of mark-up

--- src/components/SearchPanel/SearchInput/index.js
+++ src/components/SearchPanel/SearchInput/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { DummyInput } from 'CommonComponents/DummyInput';
+import { DummyButton } from 'CommonComponents/DummyButton';
 import { Switcher } from 'CommonComponents/Switcher';
 import './styles.scss';
 
@@ -12,8 +13,13 @@ export class SearchInput extends Component {
   render() {
     return (
       <div className='search-input'>
-        <DummyInput />
-        <Switcher variants={['First', 'Second']} default='First' onChange={this.handler} />
+        <div className='search-input__input'><DummyInput /></div>
+        <div className='search-input__controls'>
+          <span className='search-input__text'>Search by</span>
+          <div className='search-input__switcher'><Switcher variants={['title', 'genre']} default='title' onChange={this.handler} /></div>
+          <div className='search-input__submit'><DummyButton text='Search' isActive={true} width='100px' /></div>
+        </div>
+
       </div>
     );
   };
