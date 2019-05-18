14a669514fcf5f1e58529bd649ff909dfc9a9413
Aleksandr-Kondratskii <aleksandr_kondratskii@epam.com>
2019-05-18 19:36:16 +0400

    Task 8. @storybook/addon-knobs example added

--- src/stories/03.examples.stories.js
+++ src/stories/03.examples.stories.js
@@ -1,33 +1,55 @@
 import React from 'react';
 import Content from './content';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 
-storiesOf(Content.Examples._Chapter, module)
-    .add(
-        Content.Examples.Markdown,
-        () => (
-          <p>This is example of page with markdown in 'info'</p>
-        ),
-        { info: {
-          inline: true,
-          header: false,
-          source: false, // do not display source of code of STORY
-          text: `
-          This is markdown text.
+import { DummyButton } from 'CommonComponents/DummyButton';
 
-          # This is header
+const stories = storiesOf(Content.Examples._Chapter, module);
 
-          ## this is second level header
+stories.add(
+    Content.Examples.Markdown,
+    () => (
+      <p>This is example of page with markdown in 'info'</p>
+    ),
+    { info: {
+      inline: true,
+      header: false,
+      source: false, // do not display source of code of STORY
+      text: `
+      This is markdown text.
 
-          *Bam!*
+      # This is header
 
-          **Bam!!**
+      ## this is second level header
 
-          ~~ Вжух! ~~
+      *Bam!*
 
-          [Название места куда послать](https://www.google.com/)
+      **Bam!!**
+
+      ~~ Вжух! ~~
+
+      [Название места куда послать](https://www.google.com/)
 
 
-          `,
-        }}
-    );
+      `,
+    }}
+);
+
+stories.addDecorator(withKnobs);
+
+stories.add('DummyButton with knobs', () => {
+  const labelText = text('Label', 'Press me');
+  const isActive = boolean('Active', true);
+  const isLightened = boolean('Lightened', false);
+  const width = number('Width, px: ', 100, {
+    range: true,
+    min: 8,
+    max: 1024,
+    step: 8,
+  });
+
+  return (
+    <DummyButton text={labelText} isActive={isActive} isLight={isLightened} width={width} />
+  );
+});
